87f7849cc85fbe1f8a1bdaa8e0d488b0a2ca5e2b
BjartN <bjartn@gmail.com>
2010-03-13 13:57:21 +0100

    hello world

---
+++ Blog.ImportTumblr/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Blog.Core;
+using Blog.Infrastructure.Db4o;
+using Blog.Infrastructure.Tumblr;
+using Post=Blog.Core.Post;
+
+namespace Blog.ImportTumblr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repository = new Repository(@"C:\Users\BjartN\Documents\Visual Studio 2008\Projects\Blog\Blog\App_Data\Db4o.yap");
+            var _oldBlogs =
+                new Importer(@"C:\Users\BjartN\Documents\Visual Studio 2008\Projects\Blog\Blog.Tests\read.xml");
+             var b = new BlogSettings
+            {
+                Title = "Bjarte.Com",
+                Description = "Software architecture,design, process and business",
+                VirtualMediaPath = "~/Uploads"
+            };
+            _repository.Save(b);
+            foreach (var p in _oldBlogs.Import())
+            {
+                var post = Post.CreateLegacyPost(
+                    p.Title,
+                    p.Body, 
+                    "BjarN",
+                    p.Tags.Select(x=>new Tag(x)).ToList(),
+                    p.GmtDate,
+                    p.Url,
+                    p.Id
+                );
+
+                _repository.Save(post);
+            }
+        }
+    }
+}
