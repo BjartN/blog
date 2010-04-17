8171ceb2319951b8082b457914c27df2bd4583bb
BjartN <bjartn@gmail.com>
2010-04-17 10:11:48 +0200

    More of the updgrade

---
+++ Blog.Infrastructure/MetaWeblogApi/NoSuchPostException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Blog.Infrastructure.MetaWeblogApi
+{
+    public class NoSuchPostException : Exception
+    {
+        public NoSuchPostException(Exception inner,string id)
+            : base(string.Format("Cannot find post with id {0}",id), inner)
+        {
+        }
+    }
+}
